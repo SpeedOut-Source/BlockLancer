ab41b445b187d52142cc990afc9d759b058dfb6a
TrainerADP016 <arnob15-375@diu.edu.bd>
2023-08-04 03:36:54 +0600

    added document tsx

--- blocklancer/src/components/hero.tsx
+++ blocklancer/src/components/hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center custom-img"> 
+        <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img"> 
             <div/>
             <div>
                 <h2>Heading</h2>
--- blocklancer/src/pages/index.tsx
+++ blocklancer/src/pages/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Navbar from "../components/navbar";
 import Hero from "../components/hero";
 
+
 export default function Home() {
   return (
     <>
@@ -12,7 +13,6 @@ export default function Home() {
         <meta name="description" content="Digital Services Marketplace: Empowering Freelancers with Blockchain" />
         <link rel="icon" href="icon.png" />
       </Head>
-      <Navbar />
       <Hero />
       <p className= "text-green-500 text-center bg-blue-400">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi similique exercitationem aut corporis numquam consequuntur incidunt repudiandae minima cupiditate sit quibusdam, debitis optio impedit animi. Facilis quas eos at vitae quod aliquam exercitationem a eligendi dolore neque praesentium expedita voluptatum deserunt, quia voluptates reprehenderit. Illo esse modi error quae.
