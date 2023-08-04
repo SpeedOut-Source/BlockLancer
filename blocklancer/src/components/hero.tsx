3f709a2d6f55da9d7ddcdddf343137eac5b8c979
arnob016 <arnob.social@gmail.com>
2023-08-05 01:17:51 +0600

    hero css update & project tree err fix

--- blocklancer/src/components/hero.tsx
+++ blocklancer/src/components/hero.tsx
@@ -4,10 +4,10 @@ const Hero = () => {
     return (
         <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img"> 
             <div/>
-            <div>
+            <div className="text-white">
                 <h2>Heading</h2>
-                <p>Heading</p>
-                <button>Button</button>
+                <p className="align-middle hover:align-top">Paragraph</p>
+                <button className="px-6 py-2 border">Button</button>
             </div>
         </div>
 
--- blocklancer/src/pages/_app.tsx
+++ blocklancer/src/pages/_app.tsx
@@ -1,9 +1,5 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-<<<<<<< HEAD
 import "../styles/globals.css";
-=======
-import " /styles/globals.css";
->>>>>>> ab41b445b187d52142cc990afc9d759b058dfb6a
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
