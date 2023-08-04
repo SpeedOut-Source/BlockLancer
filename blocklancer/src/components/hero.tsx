b7020b181c9cfb464909a9716ecc248e2954441c
arnob016 <arnob.social@gmail.com>
2023-08-04 13:24:49 +0600

    update

---
+++ blocklancer/src/components/hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img"> 
+            <div/>
+            <div>
+                <h2>Heading</h2>
+                <p>Heading</p>
+                <button>Button</button>
+            </div>
+        </div>
+
+
+    );
+
+}
+
+export default Hero;
---
+++ blocklancer/src/pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Link from "next/link";
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+
+
+export default function Home() {
+  return (
+    <>
+    <main>
+      <Head>
+        <title>BlockLancer</title>
+        <meta name="description" content="Digital Services Marketplace: Empowering Freelancers with Blockchain" />
+        <link rel="icon" href="icon.png" />
+      </Head>
+      <Hero />
+      <p className= "text-green-500 text-center bg-blue-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi similique exercitationem aut corporis numquam consequuntur incidunt repudiandae minima cupiditate sit quibusdam, debitis optio impedit animi. Facilis quas eos at vitae quod aliquam exercitationem a eligendi dolore neque praesentium expedita voluptatum deserunt, quia voluptates reprehenderit. Illo esse modi error quae.
+      </p>
+
+    </main>
+   </>
+  );
+}
