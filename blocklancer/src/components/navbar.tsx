c15b8f81a12f330a591c9b27cba3498f0ce50b7f
arnob016 <arnob.social@gmail.com>
2023-08-07 14:16:21 +0600

    navbar update

--- blocklancer/src/components/navbar.tsx
+++ blocklancer/src/components/navbar.tsx
@@ -1,84 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Resources</a>
-            </li>
-            <li>
-              <a>Help</a>
-              <ul className="p-2">
-                <li>
-                  <a>FAQ</a>
-                </li>
-                <li>
-                  <a>Open Ticket</a>
-                </li>
-                <li>
-                  <a>Report a bug</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-          </ul>
-        </div>
-        <a className="btn btn-ghost normal-case text-xl">BlockLancer</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Resources</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Help</summary>
-              <ul className="space-y-3">
-                <li>
-                  <a>FAQ</a>
-                </li>
-                <li>
-                  <a>Open Ticket</a>
-                </li>
-                <li>
-                  <a>Report a bug</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>About</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Visit Site</a>
-      </div>
-    </div>
+    <div className="navbar bg-base-100 border-2 border-sky-500">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 transition duration-2000 ease-in-out hover:uppercase hover: px-5 hover:px-1 hover:py-1 hover:bg-gradient-to-l  from-pink-500 to-violet-500 ">BlockLancer</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1 flex items-center">
+      <li><a>About</a></li>
+      <li className="bg-transparent">
+        <Link href="/login" className="hover:!bg-transparent">
+          <button className="btn btn-ghost btn-outline btn-accent btn-sm bg-transparent">Visit Site</button>
+        </Link>
+      </li>
+    </ul>
+  </div>
+</div>
   );
 };
 
