ee7f77200f41d29d1a9b7c3b32a454c262ebeba0
arnob016 <arnob.social@gmail.com>
2023-08-21 13:19:49 +0600

    navbar flex fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,18 +6,17 @@ const Navbar: React.FC = () => {
   return (
     <div className="navbar bg-base-100 border-2 border-sky-500">
   <div className="flex-auto">
-    <a className="btn btn-ghost normal-case text-xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 transition duration-2000 ease-in-out hover:uppercase hover: px-5 hover:px-1 hover:py-1 hover:bg-gradient-to-l  from-pink-500 to-violet-500 ">BlockLancer</a>
-  </div>
-  <div className="flex-auto">
-
+    <a className="btn btn-ghost normal-case text-xl bg-clip-text text-transparent 
+    bg-gradient-to-r from-pink-500 to-violet-500 transition duration-2000 ease-in-out hover:bg-gradient-to-l">
+      BlockLancer</a>
   </div>
   <div className="flex-none">
-  <Search />
+    <Search />
     <ul className="menu menu-horizontal px-1 flex items-center">
       <li><a>About</a></li>
       <li className="bg-transparent">
-        <Link href="/login" className="hover:!bg-transparent">
-          <button className="btn btn-ghost btn-outline btn-accent btn-sm bg-transparent">Visit Site</button>
+        <Link href="/signup" className="hover:!bg-transparent">
+          <button className="btn btn-ghost btn-outline btn-accent btn-sm bg-transparent">SignUP</button>
         </Link>
       </li>
     </ul>
