9eb102cc1305a22dc857b8924789ee481ef135ae
Biplob Sutradhar <biplobsd11@gmail.com>
2023-09-26 10:32:36 +0600

    Basic header

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,27 +1,36 @@
 import Link from "next/link";
 import React from "react";
-import Search from "./search";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="border-2 navbar bg-base-100 border-sky-500">
-      <div className="flex-auto">
-        <a className="text-xl text-transparent normal-case transition ease-in-out btn btn-ghost bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 duration-2000 hover:bg-gradient-to-l">
+    <div className="navbar flex justify-between  bg-base-100">
+      <div className="space-x-2 bg-base-100">
+        <Link className="btn" href="/">
           BlockLancer
-        </a>
+        </Link>
+        <div className="rounded-md bg-base-200 ">
+          <Link className="btn btn-ghost" href="/">
+            Home
+          </Link>
+          <Link className="btn btn-ghost" href="/">
+            Features
+          </Link>
+          <Link className="btn btn-ghost" href="/">
+            About us
+          </Link>
+          <Link className="btn btn-ghost" href="/">
+            Contact us
+          </Link>
+        </div>
       </div>
-      <div className="flex-none">
-        <Search />
-        <ul className="flex items-center px-1 menu menu-horizontal">
-          <li><a>About</a></li>
-          <li className="bg-transparent">
-            <Link href="/signup" className="hover:!bg-transparent">
-              <button className="bg-transparent btn btn-ghost btn-outline btn-accent btn-sm">
-                Sign Up
-              </button>
-            </Link>
-          </li>
-        </ul>
+
+      <div className="space-x-2">
+        <Link className="btn" href="/">
+          Log in
+        </Link>
+        <Link className="btn" href="/">
+          Sign up
+        </Link>
       </div>
     </div>
   );
