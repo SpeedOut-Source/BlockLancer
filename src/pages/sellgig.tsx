ab26500f40f01e01e382a0c3e6e3ac26aff5c239
arnob016 <arnob.social@gmail.com>
2023-10-10 01:57:06 +0600

    image src undefined issue fix

--- src/pages/sellgig.tsx
+++ src/pages/sellgig.tsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 
 const images = ['/images/product1.jpg', '/images/product2.jpg'];
-const leftArrowImg = '/images/leftarr.png'; // Renamed variable
-const rightArrowImg = '/images/rightarr.png'; // Renamed variable
-const userImage = '/images/user.png'; // Renamed variable
+const leftArrowImg = 'https://github.com/SpeedOut-Source/BlockLancer/raw/main/public/images/leftarr.png';
+const rightArrowImg = 'https://github.com/SpeedOut-Source/BlockLancer/raw/main/public/images/rightarr.png';
+const userImage = 'https://github.com/SpeedOut-Source/BlockLancer/raw/main/public/images/user.png';
 const gigTitle = 'I can edit your photos and videos';
 
 const productTitle = 'Product Title';
 const sellerName = 'Seller Name';
-const productDetails = 'Product Details'; // Removed unnecessary "getdesc()" function
+const productDetails = 'Product Details';
 const deliveryTime = '2-3 days';
 const productAbout = [
   'A catchy intro',
@@ -35,35 +35,36 @@ const SellGig: React.FC = () => {
   const currentImage = images[currentImageIndex];
 
   return (
-      <div className="flex flex-col">
-        <div className="flex flex-col justify-start ml-10 mt-4 gap-y-4">
-          <p className="rounded-xl w-fit font-bold text-black bg-violet-400 p-2 px-10">
-            Sell Post
-          </p>
-          <p className="font-medium text-2xl text-white">{gigTitle}</p>
-          <div>
-            <div className="flex items-center">
-              <Image
-                src={userImage}
-                height={40}
-                width={40}
-                alt="user"
-                className="rounded-full mr-2"
-              />
-              <p className="text-gray-400">{sellerName}</p>
-            </div>
+    <div className="flex flex-col">
+      <div className="flex flex-col justify-start ml-10 mt-4 gap-y-4">
+        <p className="rounded-xl w-fit font-bold text-black bg-violet-400 p-2 px-10">
+          Sell Post
+        </p>
+        <p className="font-medium text-2xl text-white">{gigTitle}</p>
+        <div>
+          <div className="flex items-center">
+            <Image
+              src={userImage}
+              height={40}
+              width={40}
+              alt="user"
+              className="rounded-full mr-2"
+            />
+            <p className="text-gray-400">{sellerName}</p>
           </div>
+        </div>
 
-          <div className="flex flex-row items-center">
-            <div className="flex flex-row items-center -space-x-4">
-              <Image
-                src={leftArrowImg}
-                height={20}
-                width={20}
-                alt="left"
-                onClick={prevImage}
-                className="max-h-96 w-auto z-50 rounded-xl hover:scale-105 transition duration-500 ease-in-out cursor-pointer"
-              />
+        <div className="flex flex-row items-center">
+          <div className="flex flex-row items-center -space-x-4">
+            <Image
+              src={leftArrowImg}
+              height={20}
+              width={20}
+              alt="left"
+              onClick={prevImage}
+              className="max-h-96 w-auto z-50 rounded-xl hover:scale-105 transition duration-500 ease-in-out cursor-pointer"
+            />
+            {currentImage && (
               <Image
                 src={currentImage}
                 height={350}
@@ -71,39 +72,40 @@ const SellGig: React.FC = () => {
                 alt="Product"
                 className="z-0 max-h-96 w-auto rounded-xl"
               />
-              <Image
-                src={rightArrowImg}
-                height={20}
-                width={20}
-                alt="right"
-                onClick={nextImage}
-                className="max-h-96 w-auto z-50 rounded-xl hover:scale-105 transition duration-500 ease-in-out cursor-pointer"
-              />
-            </div>
-
-            <div className="flex flex-col w-full ml-10">
-              <h2 className="text-2xl font-bold text-white mb-2">{productTitle}</h2>
-              <h2 className="text-2xl font-light text-white mb-2">Details:</h2>
-              <p className="text-2xl font-bold text-white mb-2">{productDetails}</p>
-              <p className="text-gray-400">Delivery time: {deliveryTime}</p>
-              <button className="btn btn-outline w-fit px-7 my-2 hover:scale-105 transition duration-300 ease-in-out hover:px-10 btn-warning">
-                Confirm
-              </button>
-            </div>
+            )}
+            <Image
+              src={rightArrowImg}
+              height={20}
+              width={20}
+              alt="right"
+              onClick={nextImage}
+              className="max-h-96 w-auto z-50 rounded-xl hover:scale-105 transition duration-500 ease-in-out cursor-pointer"
+            />
           </div>
 
-          <div className='flex flex-col ml-4'>
-            <h2 className="text-2xl font-bold text-white mb-2">About work:</h2>
-            <ul className="list-disc text-gray-400 pl-5">
-              {productAbout.map((item, index) => (
-                <li key={index} className="text-gray-400">
-                  {item}
-                </li>
-              ))}
-            </ul>
+          <div className="flex flex-col w-full ml-10">
+            <h2 className="text-2xl font-bold text-white mb-2">{productTitle}</h2>
+            <h2 className="text-2xl font-light text-white mb-2">Details:</h2>
+            <p className="text-2xl font-bold text-white mb-2">{productDetails}</p>
+            <p className="text-gray-400">Delivery time: {deliveryTime}</p>
+            <button className="btn btn-outline w-fit px-7 my-2 hover:scale-105 transition duration-300 ease-in-out hover:px-10 btn-warning">
+              Confirm
+            </button>
           </div>
         </div>
+
+        <div className='flex flex-col ml-4'>
+          <h2 className="text-2xl font-bold text-white mb-2">About work:</h2>
+          <ul className="list-disc text-gray-400 pl-5">
+            {productAbout.map((item, index) => (
+              <li key={index} className="text-gray-400">
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+    </div>
   );
 };
 
