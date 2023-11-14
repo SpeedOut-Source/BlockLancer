0944d5e5a6b1e011acc73c1457e1cef495c4cf46
Biplob Sutradhar <biplobsd11@gmail.com>
2023-11-14 22:51:49 +0600

    Add nextjs-progressbar package and component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import { Titillium_Web } from "next/font/google";
 
 import "~/styles/globals.css";
 
+const NextNProgress = dynamic(() => import("nextjs-progressbar"));
 const PopupImports = dynamic(() => import("~/components/popup_imports"));
 
 const inner = Titillium_Web({ subsets: ["latin"], weight: "400" });
@@ -24,6 +25,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
         defaultTheme="dark"
         disableTransitionOnChange
       >
+        <NextNProgress
+          height={3}
+          color={"#f5f5f5"}
+          options={{ showSpinner: false }}
+        />
         <main
           className={
             "relative flex min-h-screen flex-col justify-between bg-gradient-to-r from-cyan-800 via-blue-800 to-purple-800 before:pointer-events-none before:absolute before:inset-0 before:block before:h-full before:w-full before:bg-[url('/images/bg.jpg')] before:bg-cover before:bg-no-repeat before:opacity-5 " +
